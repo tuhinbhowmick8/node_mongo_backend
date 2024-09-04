528ed3b3ff5a463adf351c263d5e729e9411ddda
Tuhin <tuhin.bhowmick8@gmail.com>
2024-09-04 14:15:31 +0530

    changes added

--- src/db/index.js
+++ src/db/index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const connectDb = async()=>{
     try{
     const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
-    console.log(`MongoDB Connected :  ${connectionInstance.host}`)
+    console.log(`MongoDB Connected :  ${connectionInstance.connection.host}`)
     }
     catch(error){
        console.log("Mongodb connection error : ", error) 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,26 @@
+
+// const asyncHandler = (fn) => async (req, res, next) =>{ 
+//     try{
+
+//         await fn(req, res, next)
+//     }
+//     catch(error){
+//         res.status(error.status || 500).json({
+//             sucess: false,
+//             message: error.message
+//         })               
+
+//     }
+//  }
+
+
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next))
+            .catch((err) => next(err));
+    };
+};
+
+
+
+export { asyncHandler }
